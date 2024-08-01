b3056298032d1399342ce401c50e3a2135dc98f6
Louis Anh Tran <louistrannamanh@gmail.com>
2024-08-01 14:03:24 +0800

    edit test

--- tests/test_env_variable.py
+++ tests/test_env_variable.py
@@ -5,8 +5,11 @@ load_dotenv()
 
 def test_env_exists():
     database_url=os.getenv('DATABASE_URL')
+    pinecone_index=os.getenv("PINECONE_INDEX")
 
     print("database_url: ",database_url)
+    print("pinecone_index: ",pinecone_index)
 
     assert database_url != None
     assert "postgresql" in database_url
+    assert "pdf" in pinecone_index
