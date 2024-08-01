904c83ec2a084966fefc8b7f69cd1316cc63b8e5
Louis Anh Tran <louistrannamanh@gmail.com>
2024-08-01 12:50:37 +0800

    edit test cases

--- tests/test_env_variable.py
+++ tests/test_env_variable.py
@@ -6,5 +6,7 @@ load_dotenv()
 def test_env_exists():
     database_url=os.getenv('DATABASE_URL')
 
+    print("database_url: ",database_url)
+
     assert database_url != None
     assert "postgresql" in database_url
