22bfeeb6931133e971a035cc2827f6013909d588
Louis <louisnananana@gmail.com>
2025-02-23 14:32:39 +0800

    matching pipeline

--- scripts/data_extraction_pipeline/prompt_template/prompt_templates.py
+++ scripts/data_extraction_pipeline/prompt_template/prompt_templates.py
@@ -4,6 +4,8 @@ Given the following conversation between user and our AI agent for a dating appl
 2. gender preferences the user is looking for.
 3. age of user
 4. age range the user of partner the user is looking for
+5. Whether the user is trolling the Chat bot 
+
 
 Instructions:
 - Based on the conversation, determine what 4 information mentioned above.
@@ -16,14 +18,53 @@ For user's gender preferences,
 - If you can not determine the gender preferences from the conversation, please return Null 
 - Your response of this field is restricted to only 4 values: Male, Female, Both, Null
 
+For trolling:
+- Your response of this field is restricted to only 3 values: true, false
+
 For user's gender
 - If you can not determine user gender, return Null
-- Your response of this field is restricted to only 3 values: Male, Female, Null
+- If user answer is Other, your response should be Other
+- Your response of this field is restricted to only 3 values: Male, Female, Null, Other
 
+For justification_trolling:
+- Give your response why this user might be trolling the AI Chat bot.
 
 ----------
-CHAT HISTORY: {chat_history}
+CHAT HISTORY: 
+{chat_history}
 
 ----------
 Your response:
+"""
+
+TEST="""
+For trolling:
+- Your response of this field is restricted to only 3 values: True, False
+
+For justification_trolling:
+- Give your response why this user might be trolling the AI Chat bot.
+
+5. Whether the user is trolling the Chat bot 
+"""
+
+
+MATCHING_TEMPLATE="""
+You are an expert in match maker, given the two user conversation history with our dating application, your task is to determine if they are compatible and give your reasons for that
+
+Instructions:
+1. Give a clear and comprehensive answer
+2. Provide reasons why you think they are a good match
+
+-------------
+USER 1 CHAT HISTORY
+{user1_chat}
+
+-------------
+USER 1 CHAT HISTORY
+{user2_chat}
+
+
+-------------
+Your response
+
 """
